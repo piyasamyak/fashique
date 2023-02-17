d0f731629b4df1249433efe6de50ac919024222d
Samyak Piya <samyakpiya@gmail.com>
2023-02-16 20:01:55 -0500

    added in our addToCart feature

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,47 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  totalCartItems: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+  const [totalCartItems, setTotalCartItems] = useState(0);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+    setTotalCartItems(totalCartItems + 1);
+  };
+
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItems,
+    totalCartItems,
+    setTotalCartItems,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,10 @@
+import "./checkout.styles.scss";
+
+const Checkout = () => {
+  // Checkout Page, different checkout items
+  // Increment the item and decrement the item using arrow
+  // Cross button to remove an item entirely
+  // Comprise the total value
+};
+
+export default Checkout;
